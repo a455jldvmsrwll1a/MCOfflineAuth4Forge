1135e064296fabb8a54679a56c50329ca0b6a6ae
a455jldvmsrwll1a <51287554+a455jldvmsrwll1a@users.noreply.github.com>
2025-02-09 19:37:19 -0600

    Add BadPackets mod.
    https://github.com/badasintended/badpackets

--- src/main/java/balls/jl/mcofflineauth4forge/MCOfflineAuth.java
+++ src/main/java/balls/jl/mcofflineauth4forge/MCOfflineAuth.java
@@ -1,11 +1,8 @@
 package balls.jl.mcofflineauth4forge;
 
 import com.mojang.logging.LogUtils;
-import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
@@ -17,16 +14,5 @@ public class MCOfflineAuth {
 
     public MCOfflineAuth(FMLJavaModLoadingContext context) {
         MinecraftForge.EVENT_BUS.register(this);
-    }
-
-    @Mod.EventBusSubscriber(modid = Constants.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-    public static class ClientModEvents {
-        @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event) {
-            // Some client setup code
-            LOGGER.info("Initialising MCOfflineAuth::Client. (on Forge)");
-
-            ClientKeyPair.loadOrCreate();
-        }
     }
 }
