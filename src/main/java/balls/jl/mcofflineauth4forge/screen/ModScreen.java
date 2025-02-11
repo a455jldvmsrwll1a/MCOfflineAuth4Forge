e2abfd123d31300af7aff183b7631889995b7997
a455jldvmsrwll1a <51287554+a455jldvmsrwll1a@users.noreply.github.com>
2025-02-11 00:03:47 -0600

    Port over mod screen from Fabric version.

---
+++ src/main/java/balls/jl/mcofflineauth4forge/screen/ModScreen.java
@@ -0,0 +1,74 @@
+package balls.jl.mcofflineauth4forge.screen;
+
+import balls.jl.mcofflineauth4forge.ClientKeyPair;
+import balls.jl.mcofflineauth4forge.Constants;
+import balls.jl.mcofflineauth4forge.util.KeyEncode;
+import com.mojang.blaze3d.platform.ClipboardManager;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.gui.components.Button;
+import net.minecraft.client.gui.components.Tooltip;
+import net.minecraft.client.gui.components.toasts.SystemToast;
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
+
+public class ModScreen extends Screen {
+    private final Screen parent;
+
+    public ModScreen(Screen parent) {
+        super(Component.empty());
+        this.parent = parent;
+    }
+
+    @Override
+    protected void init() {
+        assert this.minecraft != null;
+        int width = this.minecraft.getWindow().getGuiScaledWidth();
+        int height = this.minecraft.getWindow().getGuiScaledHeight();
+        String keyString = KeyEncode.encodePublic(ClientKeyPair.KEY_PAIR.getPublic());
+        Button pkBtnWidget = Button.builder(Component.literal(keyString), (btn) -> {
+            ClipboardManager clipboard = new ClipboardManager();
+            clipboard.setClipboard(this.minecraft.getWindow().getWindow(), keyString);
+            SystemToast.add(this.minecraft.getToastManager(), new SystemToast.SystemToastId(), Component.literal("Public key copied to clipboard!"), Component.literal("You can share this key with others."));
+        }).tooltip(Tooltip.create(Component.literal("Copy to clipboard."))).bounds(75, 150 - this.font.lineHeight - 15, width - 100 - 20, 20).build();
+
+        Button resetBtnWidget = Button.builder(Component.literal("§cRegenerate§r"), (btn) -> this.minecraft.setScreen(new WipeConfirmationScreen(this))).tooltip(Tooltip.create(Component.literal("§lWipe§r the key-pair & make a new one."))).bounds(15, height - this.font.lineHeight - 20, 80, 20).build();
+
+        Button reloadBtnWidget = Button.builder(Component.literal("Reload"), (btn) -> {
+            try {
+                ClientKeyPair.load();
+            } catch (RuntimeException e) {
+                SystemToast.add(this.minecraft.getToastManager(), new SystemToast.SystemToastId(), Component.literal("Failed to load key-pair!"), Component.literal(e.toString()));
+            }
+            this.minecraft.setScreen(new ModScreen(this.parent));
+        }).tooltip(Tooltip.create(Component.literal("Load the key from disk again. (e.g. if you changed the files while Minecraft was open.)"))).bounds(15 + 80 + 10, height - this.font.lineHeight - 20, 80, 20).build();
+
+        Button explorerBtnWidget = Button.builder(Component.literal("Open Folder"), (btn) -> net.minecraft.Util.getPlatform().openPath(Constants.MOD_DIR)).tooltip(Tooltip.create(Component.literal("Open the mod's directory with the native file manager."))).bounds(15 + 80 + 80 + 10 + 10, height - this.font.lineHeight - 20, 100, 20).build();
+        Button cancelBtnWidget = Button.builder(Component.literal("Back"), (btn) -> close()).bounds(width - 80 - 15, height - this.font.lineHeight - 20, 80, 20).build();
+
+        this.addRenderableWidget(pkBtnWidget);
+        this.addRenderableWidget(resetBtnWidget);
+        this.addRenderableWidget(reloadBtnWidget);
+        this.addRenderableWidget(explorerBtnWidget);
+        this.addRenderableWidget(cancelBtnWidget);
+    }
+
+    @Override
+    public void render(GuiGraphics gfx, int mouseX, int mouseY, float delta) {
+        super.render(gfx, mouseX, mouseY, delta);
+
+        gfx.drawString(this.font, "MC Offline Auth Configuration", 15, 40 - this.font.lineHeight - 10, 0xFFA719FF, true);
+        gfx.drawString(this.font, "By JL :>", 15, 60 - this.font.lineHeight - 10, 0xFF85F1FF, true);
+        gfx.drawString(this.font, "Allows servers to have some form of authentication without using", 15, 80 - this.font.lineHeight - 10, 0xFFFFFFFF, true);
+        gfx.drawString(this.font, "external authentication systems, lending itself to offline servers.", 15, 90 - this.font.lineHeight - 10, 0xFFFFFFFF, true);
+        gfx.drawString(this.font, "DISCLAIMER: THIS MOD IS EXPERIMENTAL SOFTWARE, IT HAS NOT UNDERGONE", 15, 105 - this.font.lineHeight - 10, 0xFFEB9B34, true);
+        gfx.drawString(this.font, "EXTENSIVE TESTING. I AM NOT RESPONSIBLE FOR ANY GRIEFED SERVERS.", 15, 115 - this.font.lineHeight - 10, 0xFFEB9B34, true);
+        gfx.drawString(this.font, "IT IS ALWAYS BETTER TO AVOID OFFLINE MODE IF POSSIBLE.", 15, 125 - this.font.lineHeight - 10, 0xFFEB9B34, true);
+        gfx.drawString(this.font, "USE THIS MOD AT YOUR OWN RISK.", 15, 135 - this.font.lineHeight - 10, 0xFFEB9B34, true);
+        gfx.drawString(this.font, "Public key: ", 15, 150 - this.font.lineHeight - 10, 0xFFFFFFFF, true);
+    }
+
+    public void close() {
+        assert this.minecraft != null;
+        this.minecraft.setScreen(parent);
+    }
+}
